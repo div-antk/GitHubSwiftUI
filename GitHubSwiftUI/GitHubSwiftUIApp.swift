44fff4d38fd29bd5474745630f4f97cea9c7039e
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-28 20:36:07 +0900

    Initial Commit

---
+++ GitHubSwiftUI/GitHubSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSwiftUIApp.swift
+//  GitHubSwiftUI
+//
+//  Created by Takuya Ando on 2021/02/28.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
