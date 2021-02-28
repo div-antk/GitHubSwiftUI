484352255e4df606fc018a3289d4577e69b9cd64
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-28 21:44:00 +0900

    NavigationBarの実装

--- GitHubSwiftUI/View/ContentView.swift
+++ GitHubSwiftUI/View/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var text = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView(showsIndicators: false) {
+                //
+            }
+        }
+        .padding()
+        .navigationBarTitle("", displayMode: .inline)
+        .navigationBarItems(leading: HStack {
+            TextField("検索ワード", text: $text, onCommit: {
+                // 編集完了後に呼ばれるクロージャー
+            })
+            // 角丸デザイン
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .keyboardType(.asciiCapable)
+            // ナビゲーションバーに置く場合は横幅設定が必要
+            .frame(width: UIScreen.main.bounds.width - 40)
+        })
     }
 }
 
