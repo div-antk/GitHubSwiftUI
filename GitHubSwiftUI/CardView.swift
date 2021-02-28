e5145d77b31541e056a73eff650212d057fa8b39
div-antk <51283468+owaii@users.noreply.github.com>
2021-02-28 20:51:07 +0900

    構造体を作成

---
+++ GitHubSwiftUI/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  GitHubSwiftUI
+//
+//  Created by Takuya Ando on 2021/02/28.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
---
+++ GitHubSwiftUI/InputModel.swift
@@ -0,0 +1,20 @@
+//
+//  InputModel.swift
+//  GitHubSwiftUI
+//
+//  Created by Takuya Ando on 2021/02/28.
+//
+
+import Foundation
+import UIKit
+
+struct Input: Identifiable {
+    
+    let id: UUID = UUID()
+    let iconImage: UIImage
+    let title: String
+    let language: String?
+    let star: Int
+    let description: String?
+    let url: String
+}
